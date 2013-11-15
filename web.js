0743e5b89699a4508e5cd14aa13685361b7764b4
Radu Vultur <raduvultur@gmail.com>
2013-11-15 10:04:37 -0500

    changed port config

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
