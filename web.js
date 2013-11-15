2a307a69b91a0cf605b3c7d9eb5f35fb756c9935
Radu Vultur <raduvultur@gmail.com>
2013-11-15 09:14:00 -0500

    removed logger

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
-// Load the http module to create an http server.
-var http = require('http');
+var express = require('express');
+var app = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(80);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
